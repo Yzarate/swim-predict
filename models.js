0c3cf83622fbff283d117ea169fa347b9dabab0b
Yzarate <86133670+Yzarate@users.noreply.github.com>
2026-02-17 09:38:40 -0500

    Add files via upload

---
+++ models.js
@@ -0,0 +1,62 @@
+export const MODELS = {
+  "G_SCY_50FR": {
+    "displayName": "Girls SCY 50 FR",
+    "slope": 0.5363862198245534,
+    "intercept": -10.903236723883255,
+    "residP25": -2.6341631622607897,
+    "residP50": 0.05698284862331082,
+    "residP75": 2.9595663648745294,
+    "minStartTime": 23.92,
+    "maxStartTime": 103.79,
+    "nPairs": 646,
+    "nSwimmers": 160
+  },
+  "G_SCY_100FR": {
+    "displayName": "Girls SCY 100 FR",
+    "slope": 0.28180303193020184,
+    "intercept": -12.789728091832991,
+    "residP25": -2.5211843824694036,
+    "residP50": -0.02185905391104903,
+    "residP75": 2.709393259533596,
+    "minStartTime": 53.35,
+    "maxStartTime": 130.94,
+    "nPairs": 565,
+    "nSwimmers": 142
+  },
+  "G_SCY_50BK": {
+    "displayName": "Girls SCY 50 BK",
+    "slope": 0.4172285672193387,
+    "intercept": -8.45003944789516,
+    "residP25": -2.850453270385115,
+    "residP50": 0.26618295971741013,
+    "residP75": 3.2395770311050534,
+    "minStartTime": 27.74,
+    "maxStartTime": 82.13,
+    "nPairs": 533,
+    "nSwimmers": 129
+  },
+  "G_SCY_100BK": {
+    "displayName": "Girls SCY 100 BK",
+    "slope": 0.2467217649342971,
+    "intercept": -12.251064385769112,
+    "residP25": -2.7361673152588115,
+    "residP50": -0.16190433194049403,
+    "residP75": 2.906846147162179,
+    "minStartTime": 56.88,
+    "maxStartTime": 171.01,
+    "nPairs": 496,
+    "nSwimmers": 135
+  },
+  "G_SCY_50BR": {
+    "displayName": "Girls SCY 50 BR",
+    "slope": 0.42205956204749184,
+    "intercept": -10.687037329735594,
+    "residP25": -3.063015451233575,
+    "residP50": -0.09994530089793319,
+    "residP75": 3.2691275270022793,
+    "minStartTime": 33.51,
+    "maxStartTime": 96.62,
+    "nPairs": 349,
+    "nSwimmers": 99
+  }
+};
